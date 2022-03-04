f7aca1cef6960e92b7b880f368f9fd383272c2d3
nu2this <git@nu2this.com>
2022-03-03 21:54:24 -0600

    combined function

--- marketQueries.py
+++ marketQueries.py
@@ -2,26 +2,21 @@ import requests
 import csv
 
 
-def getdata(ids):
+def getdata():
     print("Refreshing Data... Please Wait")
+    ids = ''
+    data = [row for row in csv.reader(open('typeids.csv'))]
+    for item in data:
+        ids += ',' + item[0]
     r = requests.get('https://api.evemarketer.com/ec/marketstat?typeid=' + ids.lstrip(','))
     with open('marketdata.xml', 'w') as f:
         f.write(r.text)
 
 
-def readcsv():
-    out = ''
-    data = [row for row in csv.reader(open('/mnt/c/Users/Mike/Documents/'\
-            'Python/eve/typeids.csv'))]
-    for item in data:
-        out += ',' + item[0]
-    return out
-
-
 if __name__ == '__main__':
     refresh = input("Would you like to refresh market data with new information? y/n")
     if refresh == 'y':
-        getdata(readcsv())
+        getdata()
 
     else:
         print("Goodbye Noob")
